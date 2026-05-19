beceb5475bdb5e2258398ebfc211f8db823194cb
Oliver Münzberg <oliver.muenzberg@gmail.com>
2026-05-19 11:09:00 +0200

    ScrollViewer scrollt zu Add-Dialog bei ShowAddDialog

--- src/otpApp/Views/MainWindow.axaml.cs
+++ src/otpApp/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using otpApp.ViewModels;
 
 namespace otpApp.Views;
 
@@ -7,5 +8,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        DataContextChanged += OnDataContextChanged;
+    }
+
+    private void OnDataContextChanged(object? sender, EventArgs e)
+    {
+        if (DataContext is MainWindowViewModel vm)
+        {
+            vm.PropertyChanged += (_, args) =>
+            {
+                if (args.PropertyName == nameof(MainWindowViewModel.ShowAddDialog) && vm.ShowAddDialog)
+                {
+                    ContentScrollViewer?.ScrollToEnd();
+                }
+            };
+        }
     }
 }
