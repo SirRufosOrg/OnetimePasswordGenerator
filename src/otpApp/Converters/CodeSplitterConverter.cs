95fcbde9b5505cde3d9ea59de7b6dabf8f9d7d61
Oliver Münzberg <oliver.muenzberg@gmail.com>
2026-05-19 14:12:52 +0200

    CodeSplitter: left-to-right Gruppierung + Formatierung

--- src/otpApp/Converters/CodeSplitterConverter.cs
+++ src/otpApp/Converters/CodeSplitterConverter.cs
@@ -1,25 +1,25 @@
 using System.Globalization;
+
 using Avalonia.Data.Converters;
 
 namespace otpApp.Converters;
 
 public class CodeSplitterConverter : IValueConverter
 {
-    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object? Convert( object? value, Type targetType, object? parameter, CultureInfo culture )
     {
-        if (value is not string code)
+        if ( value is not string code )
             return null;
 
         var parts = new List<string>();
-        for (var i = code.Length; i > 0; i -= 3)
+        for ( var i = 0; i < code.Length; i += 3 )
         {
-            var start = Math.Max(0, i - 3);
-            parts.Insert(0, code[start..i]);
+            parts.Add( code[i..Math.Min( i + 3, code.Length )] );
         }
         return parts;
     }
 
-    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object? ConvertBack( object? value, Type targetType, object? parameter, CultureInfo culture )
     {
         throw new NotSupportedException();
     }
